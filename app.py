df8f9322e660c1be7d32d7500ea81602fafc700a
szilvia-csernus <>
2023-05-11 10:11:45 +0000

    bind data from db to accordion

--- app.py
+++ app.py
@@ -21,7 +21,8 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.tasks.find()
+    # without the python list() function, it's a Mongo cursor opject
+    tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
 
 
